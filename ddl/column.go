b3ba471dcc2066bc84aff9b04c540e4c99e1d6e3
mayingchun <mayingchun321@gmail.com>
2021-09-12 15:20:08 +0800

    proj3

--- ddl/column.go
+++ ddl/column.go
@@ -219,13 +219,22 @@ func onDropColumn(t *meta.Meta, job *model.Job) (ver int64, _ error) {
 	//       (Think about how the not null property and default value will influence the `Drop Column` operation.
 	switch colInfo.State {
 	case model.StatePublic:
-		// To be filled
+		adjustColumnInfoInDropColumn(tblInfo, colInfo.Offset)
+		if mysql.HasNoDefaultValueFlag(colInfo.Flag) {
+			job.SchemaState = model.StateWriteOnly
+			colInfo.State = model.StateWriteOnly
+		} else {
+			job.SchemaState = model.StateDeleteOnly
+			colInfo.State = model.StateDeleteOnly
+		}
 		ver, err = updateVersionAndTableInfoWithCheck(t, job, tblInfo, originalState != colInfo.State)
 	case model.StateWriteOnly:
-		// To be filled
+		job.SchemaState = model.StateDeleteOnly
+		colInfo.State = model.StateDeleteOnly
 		ver, err = updateVersionAndTableInfo(t, job, tblInfo, originalState != colInfo.State)
 	case model.StateDeleteOnly:
-		// To be filled
+		job.SchemaState = model.StateDeleteReorganization
+		colInfo.State = model.StateDeleteReorganization
 		ver, err = updateVersionAndTableInfo(t, job, tblInfo, originalState != colInfo.State)
 	case model.StateDeleteReorganization:
 		// reorganization -> absent
